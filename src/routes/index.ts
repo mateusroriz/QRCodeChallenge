9a1ac0651d084163cf97031d62683296775369f5
Mateus Roriz <mateusvroriz6b@gmail.com>
2020-09-13 18:49:09 -0300

    encrypting qrcode string and sending it via POST

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express';
+import app from '../app';
 import transactionRouter from './transaction.routes';
 
 const routes = Router();
-
 routes.use('/transactions', transactionRouter);
-
+routes.get('/transactions', (req, resp)=> resp.send(transactionRouter))
 export default routes;
--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
+import bcrypt from 'bcryptjs';
 
 const transactionRouter = Router();
 
 transactionRouter.post('/', async (request, response) => {
   try {
-    // TODO
-  } catch (err) {
+    const {body} = request;
+    const qrCodeString = bcrypt.hashSync(JSON.stringify(body), 10);
+    response.json({ qrCodeString });
+  } 
+  catch (err) {
     return response.status(400).json({ error: err.message });
   }
 });
